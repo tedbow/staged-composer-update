d481d63d3e75e4217ef73e691cafa86c8e0122bc
effulgentsia <alex.bronstein@acquia.com>
2020-12-11 11:00:11 -0800

    poc3

--- src/StagedUpdateCommand.php
+++ src/StagedUpdateCommand.php
@@ -4,7 +4,6 @@ namespace Effulgentsia\StagedComposerUpdate;
 
 use Composer\Command\BaseCommand;
 use Composer\Console\Application;
-use Composer\Factory;
 use Symfony\Component\Console\Input\ArrayInput;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
@@ -19,34 +18,23 @@ class StagedUpdateCommand extends BaseCommand {
   }
 
   protected function execute(InputInterface $input, OutputInterface $output) {
-    $composer = $this->getComposer();
+    $stagedDirectory = '/tmp/staged-composer-update';
+    $excludeDirectory = 'web/sites/default/files';
 
-    $composerFile = $composer->getConfig()->getConfigSource()->getName();
-    $lockFile = Factory::getLockFile($composerFile);
-    $vendorDirectory = $composer->getConfig()->get('vendor-dir');
-    $localRepoDirectory = './_local';
-    $phpstanDirectory = './phpstan';
-
-    $stagedDirectory = '/tmp/composer';
-    $stagedComposerFile = $stagedDirectory . '/composer.json';
-    $stagedLockFile = $stagedDirectory . '/composer.lock';
-    $stagedVendorDirectory = $stagedDirectory . '/vendor';
-    $stagedLocalRepoDirectory = $stagedDirectory . '/_local';
-    $stagedPhpstanDirectory = $stagedDirectory . '/phpstan';
-
-    copy($composerFile, $stagedComposerFile);
-    copy($lockFile, $stagedLockFile);
-    exec('rsync -a --del ' . escapeshellarg($vendorDirectory . '/') . ' ' . escapeshellarg($stagedVendorDirectory . '/'));
-    exec('rsync -a --del ' . escapeshellarg($localRepoDirectory . '/') . ' ' . escapeshellarg($stagedLocalRepoDirectory . '/'));
-    exec('rsync -a --del ' . escapeshellarg($phpstanDirectory . '/') . ' ' . escapeshellarg($stagedPhpstanDirectory . '/'));
-
+    if (!is_dir($stagedDirectory)) {
+      mkdir($stagedDirectory);
+    }
+    exec('rsync -a --del --exclude=' . escapeshellarg('/' . $excludeDirectory . '/') . ' ./ ' . escapeshellarg($stagedDirectory . '/'));
     $cwd = getcwd();
     chdir($stagedDirectory);
+
     $child_app = new Application();
+    $child_app->setAutoExit(false);
     $child_input = new ArrayInput(['command' => 'update', '--prefer-lowest' => $input->getOption('prefer-lowest')], $this->getDefinition());
     $result = $child_app->run($child_input, $output);
-    chdir($cwd);
 
+    chdir($cwd);
+    exec('rsync -a --del --exclude=' . escapeshellarg('/' . $excludeDirectory . '/') . ' ' . escapeshellarg($stagedDirectory . '/') . ' ./');
     return $result;
   }
 
