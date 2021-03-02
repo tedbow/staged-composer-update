238d230ed479a20e870d98c0fdd7e3ac61d1ed2f
Ted Bowman <ted@tedbow.com>
2021-03-02 17:54:32 -0500

    allow specifiying packages

--- src/StagedUpdateCommand.php
+++ src/StagedUpdateCommand.php
@@ -5,6 +5,7 @@ namespace Effulgentsia\StagedComposerUpdate;
 use Composer\Command\BaseCommand;
 use Composer\Console\Application;
 use Symfony\Component\Console\Input\ArrayInput;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -13,13 +14,14 @@ class StagedUpdateCommand extends BaseCommand {
 
   protected function configure() {
     $this->setName('staged-update')->setDefinition([
+      new InputArgument('packages', InputArgument::IS_ARRAY | InputArgument::OPTIONAL, 'Packages that should be updated, if not provided all packages are.'),
       new InputOption('prefer-lowest', null, InputOption::VALUE_NONE, 'Prefer lowest versions of dependencies.'),
     ]);
   }
 
   protected function execute(InputInterface $input, OutputInterface $output) {
-    $stagedDirectory = '/tmp/staged-composer-update';
-    $excludeDirectory = 'web/sites/default/files';
+    $stagedDirectory = sys_get_temp_dir() . '/staged-composer-update';
+    $excludeDirectory = 'sites/default/files';
 
     if (!is_dir($stagedDirectory)) {
       mkdir($stagedDirectory);
@@ -30,7 +32,14 @@ class StagedUpdateCommand extends BaseCommand {
 
     $child_app = new Application();
     $child_app->setAutoExit(false);
-    $child_input = new ArrayInput(['command' => 'update', '--prefer-lowest' => $input->getOption('prefer-lowest')], $this->getDefinition());
+    $child_input = new ArrayInput(
+      [
+      'command' => 'update',
+      'packages' => $input->getArgument('packages'),
+      '--prefer-lowest' => $input->getOption('prefer-lowest')
+      ],
+      $this->getDefinition()
+    );
     $result = $child_app->run($child_input, $output);
 
     chdir($cwd);
