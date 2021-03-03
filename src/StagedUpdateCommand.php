11ba7b850889a55a8901f5ef8d0fdf512a33ed4b
Ted Bowman <ted@tedbow.com>
2021-03-03 07:47:45 -0500

    add mode option to allow only staging

--- src/StagedUpdateCommand.php
+++ src/StagedUpdateCommand.php
@@ -16,33 +16,48 @@ class StagedUpdateCommand extends BaseCommand {
     $this->setName('staged-update')->setDefinition([
       new InputArgument('packages', InputArgument::IS_ARRAY | InputArgument::OPTIONAL, 'Packages that should be updated, if not provided all packages are.'),
       new InputOption('prefer-lowest', null, InputOption::VALUE_NONE, 'Prefer lowest versions of dependencies.'),
+        new InputOption('mode', null, InputOption::VALUE_OPTIONAL, 'full, stage, copy-back', 'full')
     ]);
   }
 
   protected function execute(InputInterface $input, OutputInterface $output) {
+      // @todo Add staged dir option
     $stagedDirectory = sys_get_temp_dir() . '/staged-composer-update';
     $excludeDirectory = 'sites/default/files';
-
-    if (!is_dir($stagedDirectory)) {
-      mkdir($stagedDirectory);
+    $mode = $input->getOption('mode');
+    if (!in_array($mode, ['full', 'stage', 'copy-back'])) {
+        throw new \UnexpectedValueException("Unknown mode $mode");
     }
-    exec('rsync -a --del --exclude=' . escapeshellarg('/' . $excludeDirectory . '/') . ' ./ ' . escapeshellarg($stagedDirectory . '/'));
-    $cwd = getcwd();
-    chdir($stagedDirectory);
 
-    $child_app = new Application();
-    $child_app->setAutoExit(false);
-    $child_input = new ArrayInput(
-      [
-      'command' => 'require',
-      'packages' => $input->getArgument('packages'),
-      ]
-    );
-    $result = $child_app->run($child_input, $output);
+    $result = 0;
+    if ($mode === 'full' || $mode === 'stage') {
+        if (!is_dir($stagedDirectory)) {
+            mkdir($stagedDirectory);
+        }
+        exec('rsync -a --del --exclude=' . escapeshellarg('/' . $excludeDirectory . '/') . ' ./ ' . escapeshellarg($stagedDirectory . '/'));
+        $cwd = getcwd();
+        chdir($stagedDirectory);
 
-    chdir($cwd);
-    exec('rsync -a --del --exclude=' . escapeshellarg('/' . $excludeDirectory . '/') . ' ' . escapeshellarg($stagedDirectory . '/') . ' ./');
-    return $result;
+        $child_app = new Application();
+        $child_app->setAutoExit(false);
+        $child_input = new ArrayInput(
+          [
+            'command' => 'require',
+            'packages' => $input->getArgument('packages'),
+          ]
+        );
+        $result = $child_app->run($child_input, $output);
+        chdir($cwd);
+        if ($mode === 'stage') {
+            return $result;
+        }
+    }
+
+    if ($mode === 'full' || $mode === 'copy-back') {
+        exec('rsync -a --del --exclude=' . escapeshellarg('/' . $excludeDirectory . '/') . ' ' . escapeshellarg($stagedDirectory . '/') . ' ./');
+        return $result;
+    }
+
   }
 
 }
